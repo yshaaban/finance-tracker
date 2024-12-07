3b41de8e272311288159cf52e0659479b1194e06
Yasser Shaaban <yrsh@amazon.com>
2024-12-06 19:48:56 -0800

    minor fixes

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,10 +2,10 @@ import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
     err: Error,
-    req: Request,
+    _req: Request,
     res: Response,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    next: NextFunction
+    _next: NextFunction
 ) => {
     console.error(err.stack);
     res.status(500).json({ error: err.message });
